fd392f9a8a031f5b839805d3ee60ae22ca18a607
JHONATHAN ALCIDES AREVALO RODRIGUEZ <jareva16@ibero.edu.co>
2023-05-27 10:57:34 -0500

    Agregar Controlador Rol y Vistas

---
+++ ElectriLine/Controllers/rolsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ElectriLine.Models;
+
+namespace ElectriLine.Controllers
+{
+    public class rolsController : Controller
+    {
+        private ElectricLineEntities1 db = new ElectricLineEntities1();
+
+        // GET: rols
+        public ActionResult Index()
+        {
+            return View(db.rol.ToList());
+        }
+
+        // GET: rols/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // GET: rols/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: rols/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idrol,nombre,descripcion,estado")] rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.rol.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        // GET: rols/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: rols/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idrol,nombre,descripcion,estado")] rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        // GET: rols/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: rols/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            rol rol = db.rol.Find(id);
+            db.rol.Remove(rol);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
